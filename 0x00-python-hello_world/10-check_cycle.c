1a54cf4c6cd8a7a4d8ccbb07701e9d709dc8bfad
Rmolimock <495@holbertonschool.com>
2019-04-29 20:40:32 +0000

    check if linked list is circular

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *check_cycle - checks if linked list repeats
+ *@list: pointer to head of the list
+ *
+ *Return: number of nodes in the list
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *one;
+	listint_t *two;
+
+	if (list == NULL)
+		return (0);
+	one = list;
+	two = list;
+	do {
+		if (one->next == NULL || two->next->next == NULL)
+			return (0);
+		one = one->next;
+		if (two->next != NULL)
+			two = two->next->next;
+	} while (one != two);
+	return (1);
+}
