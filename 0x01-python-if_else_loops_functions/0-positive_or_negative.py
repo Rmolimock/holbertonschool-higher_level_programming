daac783de1f12c7218760fecc0bab5edd8362a65
Rmolimock <495@holbertonschool.com>
2019-04-30 20:31:02 +0000

    learning basic conditionals in python

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number == 0:
+    text = "is zero"
+elif number > 0:
+    text = "is positive"
+else:
+    text = "is negative"
+print("{:d}".format(number), text)
+
