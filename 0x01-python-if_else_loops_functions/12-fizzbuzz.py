09bb1a558dabd6927fd1a4fc9a63a88518a88e22
Rmolimock <495@holbertonschool.com>
2019-05-01 04:08:11 +0000

    Style: pep8 compliant whitespace

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-	for n in range(1, 101):
-		if not n % 3 and not n % 5:
-			print("FizzBuzz", end=" ")
-		elif not n % 3:
-			print("Fizz", end=" ")
-		elif not n % 5:
-			print("Buzz", end=" ")
-		else:
-			print("{}".format(n), end=" ")
+    for n in range(1, 101):
+        if not n % 3 and not n % 5:
+            print("FizzBuzz", end=" ")
+        elif not n % 3:
+            print("Fizz", end=" ")
+        elif not n % 5:
+            print("Buzz", end=" ")
+        else:
+            print("{}".format(n), end=" ")
