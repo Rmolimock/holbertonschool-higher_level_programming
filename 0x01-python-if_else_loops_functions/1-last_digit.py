a96558c7c1af76fc40760bb6de71cb3ebb7bb33d
Rmolimock <495@holbertonschool.com>
2019-05-01 00:46:17 +0000

    save copy of number, % doesn't work with -number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-sign = ""
-last = number % 10 if number > 0 else (number * -1) % 10
-if last == 0:
-    text = "and is 0"
-elif last > 5:
+tmp = number
+if number < 0:
+    number = -(number)
+last = number % 10
+if tmp < 0:
+    number = tmp
+    last = -(last)
+
+if last > 5:
     text = "and is greater than 5"
+elif last == 0:
+    text = "and is 0"
 elif last < 6:
     text = "and is less than 6 and not 0"
-if number < 0:
-    sign = "-"
-print("Last digit of {:d}".format(number), "is " + sign + "{:d}".format(last), text)
+
+print("Last digit of {:d} is {:d}".format(number, last), text)
