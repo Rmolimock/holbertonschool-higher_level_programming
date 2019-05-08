3273a3ea22120577d333c1f77abd9e2460bb2a5f
Rmolimock <495@holbertonschool.com>
2019-05-08 01:56:22 +0000

    Fixed. Check if idx > OR EQUAL to len(list)

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if not my_list:
-        return None
-    elif my_list[idx] < 0 or idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
     else:
         my_list[idx] = element
