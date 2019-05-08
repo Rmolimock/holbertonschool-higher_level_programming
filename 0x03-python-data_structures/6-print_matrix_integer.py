90b545cc2b6fb782c97916d183a33376385e2312
Rmolimock <495@holbertonschool.com>
2019-05-08 02:18:39 +0000

    Style: Pep8 compliant whitespace

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for y in range(len(matrix)):
         for x in range(len(matrix[y])):
-            if x < len(matrix[y]) -1:
+            if x < len(matrix[y]) - 1:
                 print("{:d} ".format(matrix[y][x]), end="")
             else:
                 print("{:d}".format(matrix[y][x]), end="")
