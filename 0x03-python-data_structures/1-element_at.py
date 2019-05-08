d1723be2c97d0ed45c8179dd036814ae8e58be1e
Rmolimock <495@holbertonschool.com>
2019-05-08 01:41:23 +0000

    check both edge cases in single condition

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx > len(my_list):
-        return None
-    n = my_list[idx]
-    if n < 1:
+    if idx > len(my_list) or idx < 0:
         return None
     else:
-        return n
+        return my_list[idx]
