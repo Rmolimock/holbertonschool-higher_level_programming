03aa3e9cf51bca93e3cb1aa0b054b630dfe262a5
Rmolimock <495@holbertonschool.com>
2019-05-21 00:51:10 +0000

    simplified with for loop

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    i = 0
-    while 1:
-        try:
-            while i < x:
-                print(my_list[i], end="")
-                i += 1
-            print()
-            break
-        except IndexError:
-            print()
-            break
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+        print()
+        return i + 1
+    except IndexError:
+        pass
+        print()
     return i
