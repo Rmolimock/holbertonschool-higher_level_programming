5ce406d4fc78a2128c27facd90baf771407f5385
Rmolimock <495@holbertonschool.com>
2019-05-21 00:54:37 +0000

    Style: pep8 compliant whitespace

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
             i += 1
         print()
-        return i 
+        return i
     except IndexError:
         pass
         print()
