317533953430c19425b48aff92dfd3f1bf3a4c91
Rmolimock <495@holbertonschool.com>
2019-05-21 02:14:54 +0000

    setting num instead of appending in try/except

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,17 +3,16 @@ def list_division(my_list_1, my_list_2, list_length):
     new = []
     for i in range(list_length):
         try:
-            new.append(my_list_1[i] / my_list_2[i])
+            num = (my_list_1[i] / my_list_2[i])
         except ZeroDivisionError:
-            new.append(0)
+            num = 0
             print('division by 0')
-            continue
         except TypeError:
-            new.append(0)
+            num = 0
             print('wrong type')
-            continue
         except IndexError:
-            new.append(0)
+            num = 0
             print('out of range')
-            continue
+        finally:
+            new.append(num)
     return new
