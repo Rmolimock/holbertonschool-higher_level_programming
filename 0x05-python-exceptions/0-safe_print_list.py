5f012df240942f95f7c486b68442e3ed05543792
Rmolimock <495@holbertonschool.com>
2019-05-21 00:53:00 +0000

    while loop covers some edge cases

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        for i in range(x):
+        i = 0
+        while i < x:
             print(my_list[i], end="")
+            i += 1
         print()
-        return i + 1
+        return i 
     except IndexError:
         pass
         print()
