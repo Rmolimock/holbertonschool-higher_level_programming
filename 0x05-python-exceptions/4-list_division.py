44a89ba9a160eb5afd8c7673680694c1ab04def3
Rmolimock <495@holbertonschool.com>
2019-05-21 02:09:58 +0000

    continue if error found

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,10 +7,13 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             new.append(0)
             print('division by 0')
+            continue
         except TypeError:
             new.append(0)
             print('wrong type')
+            continue
         except IndexError:
             new.append(0)
             print('out of range')
+            continue
     return new
