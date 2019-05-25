b08d178b5baf44400a5dc0cb6be2e375ad7531d9
Rmolimock <495@holbertonschool.com>
2019-05-25 04:14:07 +0000

    print two strings and check instance

---
+++ 0x07-python-test_driven_development/3-main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+say_my_name = __import__('3-say_my_name').say_my_name
+
+say_my_name("John", "Smith")
+say_my_name("Walter", "White")
+say_my_name("Bob")
+say_my_name("test", 12)
+try:
+    say_my_name(12, "White")
+except Exception as e:
+    print(e)
---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''This module contains one function, say_my_name
+'''
+def say_my_name(first_name, last_name=""):
+    '''say_my_name prints a string containing first_name and an optional
+    last_name
+    '''
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    elif last_name and not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    elif last_name is None:
+        raise TypeError("last_name must be a string")
+    else:
+        print("{} {}".format(first_name, last_name))
