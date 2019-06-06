dc0022b4a156c0947ea7f239e096fc55deecec8b
Rmolimock <495@holbertonschool.com>
2019-06-06 23:36:31 +0000

    load obj attributes from JSON file

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+class Student():
+    '''Student has a first/last name and age
+    '''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        elif isinstance(attrs, list):
+            new = {}
+            for s in attrs:
+                if isinstance(s, str) and s in self.__dict__:
+                    new[s] = self.__dict__[s]
+            return new
+    def reload_from_json(self, json):
+        for (k, v) in json.items():
+            self.k = v
