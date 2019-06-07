3ea419a87f4d91486710b2332aa2990cac3f7c7c
Rmolimock <495@holbertonschool.com>
2019-06-07 04:20:29 +0000

    pep8 style compliance

--- 0x0B-python-input_output/13-student.py
+++ 0x0B-python-input_output/13-student.py
@@ -6,6 +6,7 @@ class Student():
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self, attrs=None):
         if attrs is None:
             return self.__dict__
@@ -15,6 +16,7 @@ class Student():
                 if isinstance(s, str) and s in self.__dict__:
                     new[s] = self.__dict__[s]
             return new
+
     def reload_from_json(self, json):
         for (k, v) in json.items():
             self.k = v
