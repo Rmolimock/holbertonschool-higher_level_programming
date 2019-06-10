9043e5f8887c1238e7cdbf596ae10e1464b8b705
Rmolimock <495@holbertonschool.com>
2019-06-10 20:04:05 +0000

    validate width height x and y in setters

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -27,13 +27,33 @@ class Rectangle(Base):
         return self.__y
     @width.setter
     def width(self, value):
-        self.__width = value
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        elif value < 1:
+            raise ValueError('width must be > 0')
+        else:
+            self.__width = value
     @height.setter
     def height(self, value):
-        self.__height = value
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value < 1:
+            raise ValueError('height must be > 0')
+        else:
+            self.__height = value
     @x.setter
     def x(self, value):
-        self.__x = value
-    y.setter
+        if not isinstance(value, int):
+            raise TypeError('x must be an integer')
+        elif value < 0:
+            raise ValueError('x must be >= 0')
+        else:
+            self.__x = value
+    @y.setter
     def y(self, value):
-        self.__y = value
+        if not isinstance(value, int):
+            raise TypeError('y must be an integer')
+        elif value < 0:
+            raise ValueError('y must be >= 0')
+        else:
+            self.__y = value
