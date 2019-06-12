92e56556c0cdb7fbe8108a4bf957a77a5f060b2d
Rmolimock <495@holbertonschool.com>
2019-06-12 22:32:55 +0000

    convert dict to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """This module contains one class, Base"""
 
 
+import json
+
+
 class Base():
     """Base class contains the number of instances,
     each of which are assigned an id in assending order"""
@@ -13,3 +16,15 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or list_dictionaries == []:
+            return '"[]"'
+        elif not isinstance(list_dictionaries, list):
+            raise TypeError('list_dictionaries must be a list of dicts')
+        elif len(list_dictionaries) < 0:
+            raise ValueError('list_dictionaries must contain dictionaries')
+        elif not isinstance(list_dictionaries[0], dict):
+            raise ValueError('list_dictionaries must contain dictionaries')
+        else:
+            return json.dumps(list_dictionaries)
