15da33dd295e8dfa9f446b609b0a4ad9c2afc8e2
Rmolimock <495@holbertonschool.com>
2019-06-12 23:59:47 +0000

    holberton requirement test

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,52 +1,69 @@
 #!/usr/bin/python3
-"""This module contains one class, Base"""
+"""This module contains the base class
+"""
+import json 
 
 
-import json
-
-
-class Base():
-    """Base class contains the number of instances,
-    each of which are assigned an id in assending order"""
+class Base:
+    """This is the base class
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        ''' '''
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
-        ''' '''
-        if list_dictionaries is None or list_dictionaries == []:
-            return '"[]"'
-        elif not isinstance(list_dictionaries, list):
-            raise TypeError('list_dictionaries must be a list of dicts')
-        elif len(list_dictionaries) < 0:
-            raise ValueError('list_dictionaries must contain dictionaries')
-        elif not isinstance(list_dictionaries[0], dict):
-            raise ValueError('list_dictionaries must contain dictionaries')
-        else:
-            return json.dumps(list_dictionaries)
+        """returns JSON string representation of obj"""
+        if list_dictionaries is None or len(list_dictionaries) is 0:
+            return '[]'
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        ''' '''
-        if not isinstance(list_objs, list):
-            raise TypeError('list_objs must be a list of objects')
-        obj_class = list_objs[0].__class__
-        if not obj_class == cls:
-            raise TypeError('objects in list_objs must be same type')
-        list_dicts = []
-        for obj in list_objs:
-            if not type(obj) == obj_class:
-                raise TypeError('objects in list_objs must be of same type')
-            else:
-                obj_dict = obj.to_dictionary()
-                list_dicts.append(obj_dict)
-        json_string = cls.to_json_string(list_dicts)
-        filename = type(list_objs[0]).__name__ + '.json'
-        with open(filename, mode="w+", encoding="utf-8") as f:
-            return f.write(json_string)
+        """save json file"""
+        if list_objs is None:
+            with open("{}.json".format(cls.__name__), "w") as f:
+                f.write(to_json_string([]))
+        else:
+            tmp = []
+            with open("{}.json".format(cls.__name__), "w") as f:
+                for i in list_objs:
+                    if issubclass(type(i), Base):
+                        tmp.append(i.to_dictionary())
+                f.write(Base.to_json_string(tmp))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """return deserialized list"""
+        if json_string is None or len(json_string) is 0:
+            return "[]"
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """create new instance of subclass using
+        that class' update function
+        """
+        tmp = cls(1, 1)
+        if tmp is not None:
+            tmp.update(**dictionary)
+        return tmp
+
+    def load_from_file(cls):
+        """create instances from a JSON file
+        """
+        new = []
+        try:
+            with open("{}.json".format(cls.__name__), "r") as f:
+                tmp_dict = Base.from_json_string(f.read())
+                for inst in tmp_dict:
+                    new.append(cls.create(**inst))
+                return new
+        except Exception:
+            return []
