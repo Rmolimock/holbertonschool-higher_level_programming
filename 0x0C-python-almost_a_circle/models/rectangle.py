6d15bd5a3f173f4ee7d2be5006f7d6d1f374c4b7
Rmolimock <495@holbertonschool.com>
2019-06-13 00:31:53 +0000

    docstrings for each method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -16,22 +16,27 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        '''getter for self.width'''
         return self.__width
 
     @property
     def height(self):
+        '''getter for self.height'''
         return self.__height
 
     @property
     def x(self):
+        '''getter for self.x'''
         return self.__x
 
     @property
     def y(self):
+        '''getter for self.y'''
         return self.__y
 
     @width.setter
     def width(self, value):
+        '''setter '''
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
         elif value < 1:
@@ -41,6 +46,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        '''setter for self.height'''
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
         elif value < 1:
@@ -50,6 +56,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        '''setter for self.x'''
         if not isinstance(value, int):
             raise TypeError('x must be an integer')
         elif value < 0:
@@ -59,6 +66,7 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        '''setter for self.y'''
         if not isinstance(value, int):
             raise TypeError('y must be an integer')
         elif value < 0:
@@ -67,10 +75,12 @@ class Rectangle(Base):
             self.__y = value
 
     def __str__(self):
+        '''return a brief description of the rectangle'''
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
                 self.id, self.x, self.y, self.width, self.height)
 
     def update(self, *args, **kwargs):
+        '''update the object with new attribute values'''
         for (i, arg) in enumerate(args):
             if i == 0:
                 self.id = int(arg)
@@ -96,12 +106,14 @@ class Rectangle(Base):
                     self.y = int(v)
 
     def area(self):
+        '''return the area of the rectangle'''
         if self.width == 0 or self.height == 0:
             return 0
         else:
             return self.width * self.height
 
     def display(self):
+        '''print the rectangle in hash symbols'''
         ret = ''
         if self.width == 0 or self.height == 0:
             print(ret)
@@ -115,6 +127,7 @@ class Rectangle(Base):
         print(ret, end="")
 
     def to_dictionary(self):
+        '''return a dictionary of attributes'''
         new = {}
         new['id'] = self.id
         new['width'] = self.width
