0a0003be574dd313549fefc1c4c365fa2cce01a0
Rmolimock <495@holbertonschool.com>
2019-06-13 00:16:21 +0000

    docstrings with descriptions in each method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module contains the base class
 """
-import json 
+import json
 
 
 class Base:
@@ -15,3 +15,38 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        '''return a json string of list_dictionaries'''
+        if list_dictionaries is None or list_dictionaries == []:
+            return '"[]"'
+        elif not isinstance(list_dictionaries, list):
+            raise TypeError('list_dictionaries must be a list of dicts')
+        elif len(list_dictionaries) < 0:
+            raise ValueError('list_dictionaries must contain dictionaries')
+        elif not isinstance(list_dictionaries[0], dict):
+            raise ValueError('list_dictionaries must contain dictionaries')
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''save a list of dictionaries of objects in list_objs 
+        to a json file
+        '''
+        if not isinstance(list_objs, list):
+            raise TypeError('list_objs must be a list of objects')
+        obj_class = list_objs[0].__class__
+        if not obj_class == cls:
+            raise TypeError('objects in list_objs must be same type')
+        list_dicts = []
+        for obj in list_objs:
+            if not type(obj) == obj_class:
+                raise TypeError('objects in list_objs must be of same type')
+            else:
+                obj_dict = obj.to_dictionary()
+                list_dicts.append(obj_dict)
+        json_string = cls.to_json_string(list_dicts)
+        filename = type(list_objs[0]).__name__ + '.json'
+        with open(filename, mode="w+", encoding="utf-8") as f:
+            return f.write(json_string)
