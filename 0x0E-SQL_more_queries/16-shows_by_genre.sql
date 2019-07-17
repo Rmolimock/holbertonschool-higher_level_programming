2c38871568270e5cbde8a7f1d0493afbadd0885e
Rmolimock <495@holbertonschool.com>
2019-07-17 03:58:02 +0000

    "$cm"

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+#!/usr/bin/env mysql
+-- list shows and their genres
+-- one per line
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_genres.name
+
