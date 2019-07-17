9dce73a8e7c802251fe7ec5e721145a426f2057d
Rmolimock <495@holbertonschool.com>
2019-07-17 03:58:02 +0000

    "$cm"

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+#!/usr/bin/env mysql
+-- list all shows with >= 1 linked genre
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC
+
