baa77114fe0787a56d1dc74e5bb2225f4b0715bb
Rmolimock <495@holbertonschool.com>
2019-07-17 03:58:02 +0000

    "$cm"

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+#!/usr/bin/env mysql
+-- each genre of the show Dexter
+SELECT tv_genres.name 'name'
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY name ASC
+
