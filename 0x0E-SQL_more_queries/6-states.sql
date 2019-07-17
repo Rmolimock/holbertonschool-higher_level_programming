cb9697749d75905381c4f3e1353283da77f36bd0
Rmolimock <495@holbertonschool.com>
2019-07-17 03:58:03 +0000

    "$cm"

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+#!/usr/bin/env mysql
+-- create database with table that has unique ID's for rows
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states
+(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
+name VARCHAR(256) NOT NULL);
+
