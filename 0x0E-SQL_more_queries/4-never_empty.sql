7ab1e5db31d1092534ea5e90cbe64df2427e63eb
Rmolimock <495@holbertonschool.com>
2019-07-17 18:25:32 +0000

    "$(cat cm)"

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
 #!/usr/bin/env mysql
--- create a table with an id and name, where id can not be NULL
+-- create table with an id and name, where id can not be NULL
 CREATE TABLE IF NOT EXISTS id_not_null
 (id INT DEFAULT 1, name VARCHAR(256) NOT NULL)
 
