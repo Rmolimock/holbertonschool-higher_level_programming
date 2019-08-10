43723fffca7bc882bf55796d4855d5d5b1d8af87
Rmolimock <495@holbertonschool.com>
2019-08-10 22:11:44 +0000

    pep8 style compliant

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     connection = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
                                  user=argv[1], passwd=argv[2], db=argv[3])
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM states WHERE BINARY name ='{}' ".format(argv[4]) +
-                   "ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE BINARY name ='{}' "
+                    .format(argv[4]) + "ORDER BY id ASC")
     rows = cursor.fetchall()
 
     for eachRow in rows:
