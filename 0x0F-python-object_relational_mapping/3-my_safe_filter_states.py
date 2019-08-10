3eafb96a5c7b1bd06f52daee956a6410981f6611
Rmolimock <495@holbertonschool.com>
2019-08-10 22:25:23 +0000

    protect against sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+    Lists all states from a given database with given name
+    protect against sql injection
+'''
+import MySQLdb
+from sys import argv
+if __name__ == "__main__":
+    connection = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                                 user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+
+    names = {}
+    for eachRow in rows:
+        names[eachRow[1]] = eachRow[1]
+
+    if argv[4] in names:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM states WHERE BINARY name ='{}' "
+                       .format(argv[4]) + "ORDER BY id ASC")
+        rows = cursor.fetchall()
+        for eachRow in rows:
+            print(eachRow)
+        cursor.close()
+        connection.close()
