74c5fb0d2637ffd0f32af1050fc7020424f2336b
Rmolimock <495@holbertonschool.com>
2019-08-10 21:51:07 +0000

    trying against checker without mysql filter, python only

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,8 +9,7 @@ if __name__ == "__main__":
     connection = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
                                  user=argv[1], passwd=argv[2], db=argv[3])
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM states WHERE name REGEXP '^N'"
-                   "ORDER BY states.id ASC")
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = cursor.fetchall()
 
     for eachRow in rows:
