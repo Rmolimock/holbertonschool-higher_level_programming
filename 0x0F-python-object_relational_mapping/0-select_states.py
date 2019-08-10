445adbdbf32dd48fafc0ca7e1d8a440ef6aae8f4
Rmolimock <495@holbertonschool.com>
2019-08-10 21:02:41 +0000

    close connection after use

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,19 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+'''
+    Lists all states from a given database
+'''
 import MySQLdb
 from sys import argv
 
+if __name__ == "__main__":
+    connection = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                                 user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cursor.fetchall()
 
-if __name__ == '__main__':
-    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-                           user=argv[1], passwd=argv[2], db=argv[3])
-    c = conn.cursor()
-    c.execute("SELECT * FROM states ORDER BY states.id ASC")
-    rows = c.fetchall()
-    for each in rows:
-        print(each)
+    for eachRow in rows:
+        print(eachRow)
+
+    cursor.close()
+    connection.close()
