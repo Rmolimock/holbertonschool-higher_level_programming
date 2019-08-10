a590d2b0d332727ecb86c5d67cdbc3c47a997972
Rmolimock <495@holbertonschool.com>
2019-08-10 21:58:29 +0000

    search for given row value

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+    Lists all states from a given database starting with N
+'''
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                                 user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cursor.fetchall()
+
+    for eachRow in rows:
+        if eachRow[1] == argv[4]:
+            print(eachRow)
+
+    cursor.close()
+    connection.close()
