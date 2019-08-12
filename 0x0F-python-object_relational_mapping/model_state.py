afa929bc1d79d62f8dc625089ef05a063eb09d04
Rmolimock <495@holbertonschool.com>
2019-08-12 00:56:07 +0000

    state class that persists in database

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State class represents a state"""
+    __tablename__ = 'state'
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement="auto")
+    name = Column(String(128), nullable=False)
