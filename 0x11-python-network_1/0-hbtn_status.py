2eb59866a872f769ed405f76cb532c44231c8d57
Rmolimock <495@holbertonschool.com>
2019-08-26 16:56:27 +0000

    fetch URL and print info in given format

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# fetches a url using urllib
+from urllib import request
+
+
+if __name__ == "__main__":
+  with request.urlopen('https://intranet.hbtn.io/status') as r:
+    print('Body response:')
+    response = r.read()
+    print('\t- type: {}'.format(type(response)))
+    print('\t- content: {}'.format(response))
+    print('\t- utf8 content: {}'.format(response.decode('utf-8')))
