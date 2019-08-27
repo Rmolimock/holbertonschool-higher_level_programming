f5988bfadf739ada92f7c2213a5689ae37faca66
Rmolimock <495@holbertonschool.com>
2019-08-27 01:16:01 +0000

    send post request with parameter that has given value

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    value = sys.argv[1] if len(sys.argv) > 1 else value = ""
+    param = {"q": value}
+    response = requests.post(url, param)
+    try:
+        user = response.json()
+        if (len(user) < 1):
+            print("No result")
+        else:
+            print("[{}] {}".format(user["id"], user["name"]))
+    except:
+        print("Not a valid JSON")
