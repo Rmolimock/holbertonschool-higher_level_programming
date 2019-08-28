0b2ef6107ad8333c3a9a7e7807ba16f3a6c746e8
Rmolimock <495@holbertonschool.com>
2019-08-28 07:49:06 +0000

    print None if authentication fails

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,8 +2,10 @@
 # access github API and print user id
 import requests
 import sys
-from requests.auth import HTTPBasicAuth
 
-credentials = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+credentials = requests.auth.HTTPBasicAuth(sys.argv[1], sys.argv[2])
 response = requests.get('http://api.github.com/user', auth=credentials)
-print(response.json()['id'])
+try:
+    print(response.json()['id'])
+except KeyError:
+    print("None")
