00c0bb06aefa34384e3e5dac46d49d0ec78c5799
Rmolimock <495@holbertonschool.com>
2019-08-28 03:18:07 +0000

    pep8 style compliant

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,9 @@ from urllib import request
 
 
 if __name__ == "__main__":
-  with request.urlopen('https://intranet.hbtn.io/status') as r:
-    print('Body response:')
-    response = r.read()
-    print('\t- type: {}'.format(type(response)))
-    print('\t- content: {}'.format(response))
-    print('\t- utf8 content: {}'.format(response.decode('utf-8')))
+    with request.urlopen('https://intranet.hbtn.io/status') as r:
+        print('Body response:')
+        response = r.read()
+        print('\t- type: {}'.format(type(response)))
+        print('\t- content: {}'.format(response))
+        print('\t- utf8 content: {}'.format(response.decode('utf-8')))
